fc07ec5c8e4c9505ec1b81f726b067c141f5486b
Rawan <53065913+rawansilva999@users.noreply.github.com>
2024-02-18 06:50:39 +0000

    Pending changes exported from your codespace

--- src/events/join.py
+++ src/events/join.py
@@ -5,4 +5,4 @@ from config.config import loggers
 async def on_join(bot, user: User) -> None:
     if loggers.joins:
         print(f"User joined: {user.username}:{user.id}")
-    await bot.highrise.chat(f"🎉 Olá {user.username} seja bem vindo (a) a LOJINHA DO G4MI 🤩")
+    await bot.highrise.chat(f"🎉 Olá {user.username} seja bem vindo (a) a LOJINHA DO G4MI! 📢 Os valores dos grabs são apenas ilustrativos caso você goste de algum item e queira comprar entre em contato com os proprietários dos grabs.🤩")
